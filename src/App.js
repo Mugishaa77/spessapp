fa33c05780b818909a8fd9dd50fc11e1ba310cd3
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2024-04-17 19:57:30 +0300

    Added browser router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+      {/*Navbar*/}
+
+      <Routes>
+        {/* Format <Route path="/" exact element = {<Home />} /> */}
+
+
+        {/* Redirect to /not-found for all other routes */}
+          {/* <Route path="*" element={<Navigate to="/not-found" />} /> */}
+          {/* <Route path="/not-found" element={<NotFound />} /> */}
+
+      </Routes>
+
+
+     </BrowserRouter> 
     </div>
   );
 }
