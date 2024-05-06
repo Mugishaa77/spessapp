2ad6d1e7f760f95267fa11db6cef021ad795230f
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2024-05-07 00:27:17 +0300

    added context to chats

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,9 +1,40 @@
+import One from '../images/one.jfif';
 import End from '../components/End';
 
 export default function Messages (){
     return (
         <div className="messages">
-            <div className="inbox"></div>
+            <div className="chat-preview">
+                <h5>Messages</h5>
+                <span>Placeholder for Read and Unread</span>
+                <ul>
+                    <li>
+                        <div className="message-preview">
+                            <div className="profile-photo">
+                                <img src={One} />
+                            </div>
+                            <div className="text-preview">
+                                <h6 className="inter-text">Lorem Ipsum</h6>
+                                 <p className="inter-text">Lorem ipsum dolor sit amet
+                                    , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>                                
+                                
+                            </div>
+                            <div className="notif">
+                               
+                                <h2 className="time-stamp">10 hours ago</h2>
+                                <p className="unread inter-text">2</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+            <div className="chat-screen">
+                <span className="sender-name"> 
+                <img /> 
+                <h6 className="inter-text">Lorem Ipsum</h6></span>
+
+            </div>
             <div className="end">
                 <End />
             </div>
